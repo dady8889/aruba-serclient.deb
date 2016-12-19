15f0a6874f5b05be3eac5dea2fe6d6e87b24e71f
Viktor Szépe <viktor@szepe.net>
2016-12-19 00:49:35 +0000

    Added gitignore, remove tar files during build

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+#
+# Build Debian package from files.
+#
 
 VERSION="0.01"
 DEBIAN_RELEASE="2"
 
-cd DEBIAN
+cd CONTENTS
 GZIP="-9" tar czvf ../data.tar.gz *
 cd -
 
-cd CONTENTS
+cd DEBIAN
 GZIP="-9" tar czvf ../control.tar.gz *
 cd -
 
 ar rv aruba-serclient_${VERSION}-${DEBIAN_RELEASE}_all.deb debian-binary control.tar.gz data.tar.gz
+rm -f control.tar.gz data.tar.gz
 
 lintian --display-info --display-experimental --pedantic --show-overrides --no-tag-display-limit --verbose aruba-serclient_${VERSION}-${DEBIAN_RELEASE}_all.deb
