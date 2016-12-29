b2f062d3e43b757c6596d77d9c2482500535008e
Viktor Szépe <viktor@szepe.net>
2016-12-29 23:45:30 +0000

    Cosmetics and fix /var/log/serclient.log permissions

--- build.sh
+++ build.sh
@@ -4,20 +4,23 @@
 #
 
 VERSION="0.01"
-DEBIAN_RELEASE="2"
+DEBIAN_RELEASE="3"
 
 set -e
 
-cd CONTENTS
-find usr -type f -exec md5sum "{}" ";" > ../DEBIAN/md5sums
-GZIP="-9 -n" tar czvf ../data.tar.gz *
-cd -
+(
+    cd CONTENTS
+    find usr -type f -exec md5sum "{}" ";" > ../DEBIAN/md5sums
+    GZIP="-9 -n" tar czvf ../data.tar.gz ./*
+)
 
-cd DEBIAN
-GZIP="-9 -n" tar czvf ../control.tar.gz *
-cd -
+(
+    cd DEBIAN
+    GZIP="-9 -n" tar czvf ../control.tar.gz ./*
+)
 
 ar rv aruba-serclient_${VERSION}-${DEBIAN_RELEASE}_all.deb debian-binary control.tar.gz data.tar.gz
 rm -f control.tar.gz data.tar.gz
 
-lintian --display-info --display-experimental --pedantic --show-overrides --no-tag-display-limit --verbose aruba-serclient_${VERSION}-${DEBIAN_RELEASE}_all.deb
+lintian --display-info --display-experimental --pedantic --show-overrides --no-tag-display-limit --verbose \
+    aruba-serclient_${VERSION}-${DEBIAN_RELEASE}_all.deb
